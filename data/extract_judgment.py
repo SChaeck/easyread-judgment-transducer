9a2152106bc3c1fc55f9f4592b145f15b2394075
Suchae Jeong <zmfhtmgjsej@dgu.ac.kr>
2024-07-19 23:15:37 +0900

    feat: 데이터 전처리 완료

--- data/extract_judgment.py
+++ data/extract_judgment.py
@@ -3,12 +3,19 @@ import json
 import csv
 from tqdm import tqdm
 
+empty = 0
+
 def extract_text_from_json(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
         판시사항 = data.get("판시사항", "")
         판결요지 = data.get("판결요지", "")
         판례내용 = data.get("판례내용", "")
+        재결요지 = data.get("재결요지", "")
+        주문 = data.get("주문", "")
+        청구취지 = data.get("청구취지", "")
+        이유 = data.get("이유", "")
+        
         combined_text = ""
         if 판시사항:
             combined_text += 판시사항
@@ -16,7 +23,18 @@ def extract_text_from_json(file_path):
             combined_text += 판결요지
         if 판례내용:
             combined_text += 판례내용
-
+        if 재결요지:
+            combined_text += 재결요지
+        if 주문:
+            combined_text += 주문
+        if 청구취지:
+            combined_text += 청구취지
+        if 이유:
+            combined_text += 이유
+            
+        if combined_text == "":
+            empty += 1
+            
         return combined_text
 
 def process_json_files_in_folder(folder_path, output_csv_path):
@@ -32,10 +50,11 @@ def process_json_files_in_folder(folder_path, output_csv_path):
                     if file.endswith('.json'):
                         json_file_path = os.path.join(root, file)
                         combined_text = extract_text_from_json(json_file_path)
-                        csv_writer.writerow(['_', combined_text, '_'])
                         
-                        total_length += len(combined_text)
-                        file_count += 1
+                        if combined_text != "":                        
+                            csv_writer.writerow(['_', combined_text, '_'])
+                            total_length += len(combined_text)
+                            file_count += 1
                 except:
                     print('file name:', dirs, file)
     
@@ -49,3 +68,4 @@ def process_json_files_in_folder(folder_path, output_csv_path):
 folder_path = '115.법률-규정 텍스트 분석 데이터_고도화_상황에 따른 판례 데이터'  # JSON 파일이 있는 폴더 경로로 변경하세요
 output_csv_path = 'raw_data/judgment.csv'  # 출력 CSV 파일 경로
 process_json_files_in_folder(folder_path, output_csv_path)
+print(empty)
